a6657a4221a3e8a0778441780939a458d449c70a
antrikshmisri <antrikshmisri@gmail.com>
2021-02-22 14:20:54 +0530

    initialized repo

---
+++ main.py
@@ -0,0 +1,20 @@
+import eel
+import sys
+
+@eel.expose
+def getpara(arg):
+    return f"You passed {arg}"
+
+
+if __name__ == '__main__':
+    if sys.argv[1] == '--develop':
+        eel.init('client')
+        eel.start({
+            'port': 3000,
+        }, options={
+            'port': 8888,
+            'host': 'localhost',
+        } , suppress_error = True)
+    else:
+        eel.init('build')
+        eel.start('index.html')
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React , {Component , useState} from "react"
+import { eel } from "./eel.js";
+
 
 function App() {
+  let [value , setValue] = useState('')
+  eel.set_host("ws://localhost:8888");
+  eel.getpara("argument")(res => setValue(value = res))
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="Main">
+          <h1>Hello</h1>
+          <p>{value}</p>
+        </div>
     </div>
   );
 }
-
 export default App;
