e6ac3e2ad23aaa5817c8a7322cc410347a68867c
antrikshmisri <antrikshmisri@gmail.com>
2021-02-22 20:44:37 +0530

    feat: button component

---
+++ src/components/button.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Button = (props) => {
+    return (
+        <button className='button' onClick={props.onClick}>{props.text}</button>
+    );
+}
+export default Button
