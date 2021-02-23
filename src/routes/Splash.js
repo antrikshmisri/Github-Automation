efdd89c3a3687335cba6ef442cdd3b31b10081f7
antrikshmisri <antrikshmisri@gmail.com>
2021-02-23 15:51:19 +0530

    feat: added content to splash screen

--- src/routes/Splash.js
+++ src/routes/Splash.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import Button from '../components/button'
 import {useHistory} from 'react-router-dom'
+import {Container , Row , Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from '../assets/logo.svg'
 const Splash = () => {
     const history = useHistory()
     const nextPage = () => {
@@ -8,10 +11,17 @@ const Splash = () => {
     history.push(page)
     }
     return (
-        <>
-            <h1>Splash Screen</h1>
-            <Button text='Splash' onClick = {nextPage}/>
-        </>
+        <Container className="splash-container">
+            <Row>
+                <img className="logo" src={logo}></img>
+                <Col className="text-center" lg={"12"}>
+                    <h1 style={{marginBottom: "9.5rem"}}>GITHUB AUTOMATION</h1>
+                </Col>
+                <Col className="btn-div" lg={"12"}>
+                    <Button onClick={nextPage} text="START" bgColor="#48BFE3"/>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 
