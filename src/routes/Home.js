1209d5fca5736a77d1b44c6e04e5ce97033faa4a
antrikshmisri <antrikshmisri@gmail.com>
2021-02-24 14:03:27 +0530

    refac: formatted the code

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,32 @@
-import React from 'react'
-import Button from '../components/button'
-import {useHistory} from 'react-router-dom'
-import {Container , Row , Col} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-
+import React from "react";
+import Button from "../components/button";
+import { useHistory } from "react-router-dom";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "../components/Header.js";
+import Footer from "../components/Footer.js";
 
 const Home = () => {
-    const history = useHistory()
-    const nextPage = () => {
-        let page = '/Commit'
-        history.push(page)
-        }
-    return (
-        <div className = "text">
+  const history = useHistory();
+  const nextPage = () => {
+    let page = "/Commit";
+    history.push(page);
+  };
+  return (
+    <>
+      <Header />
+      <div className="text">
         <h1>Listening...</h1>
-        <Button onClick={nextPage} text="VIEW FILES" textColor = "#CED4DA" bgColor="#343A40"/>
-        </div>
-    );
-}
+        <Button
+          onClick={nextPage}
+          text="VIEW FILES"
+          textColor="#CED4DA"
+          bgColor="#343A40"
+        />
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Home
-
+export default Home;
