599fe83607c3cdb80afeef19dc3253ac0402a731
antrikshmisri <antrikshmisri@gmail.com>
2021-03-03 13:04:35 +0530

    fix: fixed diff formatting

--- src/scripts/diffcalc.py
+++ src/scripts/diffcalc.py
@@ -1,6 +1,5 @@
-from .colors import logcolors
 from difflib import ndiff
-from .utils import getMaxSpaces
+
 
 
 
@@ -9,15 +8,4 @@ def calcDiff(firstFile, secondFile):
     diff = ndiff(firstFile, secondFile)
     # calculate unique lines in secondfile
     deltainit = ''.join(x[2:] for x in diff if x.startswith('+ '))
-    # reformat the lines
-    deltainit = deltainit.split('\n')
-    maxspacesinit = getMaxSpaces(deltainit)
-    print(f'{logcolors.BOLD}CHANGED LINES ARE{logcolors.ENDC}\n',
-          f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
-
-    for ele in deltainit:
-        print(f'{logcolors.SUCCESS} {str(ele.strip())} {logcolors.ENDC}',
-              ' ' * (maxspacesinit - len(ele.strip())), '+')
-              
-    print('', f'{logcolors.BOLD}-{logcolors.ENDC}' * maxspacesinit)
     return deltainit
