248ce0b64fc4cb393c918aa959bc5b9af589d017
antrikshmisri <antrikshmisri@gmail.com>
2021-03-03 13:02:07 +0530

    feat: dynamically generate cards from tmp.json

--- src/routes/Commit.js
+++ src/routes/Commit.js
@@ -1,32 +1,34 @@
-import React from "react"
+import React from "react";
 import Button from "../components/button";
-import { Container, Row , Carousel , Col} from "react-bootstrap";
+import { Container, Row, Carousel, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
-import Card from '../components/card'
+import Card from "../components/card";
+import tmp from "../scripts/tmp.json";
 
 const Commit = () => {
-    return (
-        <>
-        <Header />
-        <Container>
-            <Row>
-                <Col lg={"12"}>
-                <Carousel indicators={false}>
-                    <Carousel.Item>
-                        <Card heading="Test-Card" content="Test content paragraph" />
-                    </Carousel.Item>
-                    <Carousel.Item>
-                        <Card heading="Test-Card" content="Test content paragraph" />
-                    </Carousel.Item>
-                </Carousel>
-                </Col>
-            </Row>
-        </Container>
-        <Footer />
-        </>
-    );
-}
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row>
+          <Col lg={"12"}>
+            <Carousel indicators={false}>
+              {tmp.map((file,idx) => {
+                return (
+                  <Carousel.Item key={idx}>
+                    <Card key={idx} heading={file.path.split('\\').pop()} content={file.changes} />
+                  </Carousel.Item>
+                );
+              })}
+            </Carousel>
+          </Col>
+        </Row>
+      </Container>
+      <Footer />
+    </>
+  );
+};
 
-export default Commit
+export default Commit;
