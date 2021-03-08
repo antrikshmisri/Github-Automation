90e3cca1c84469bef5e232dd0c3c9260f1560086
antrikshmisri <antrikshmisri@gmail.com>
2021-03-09 03:05:13 +0530

    feat: eel commit and update on submit

--- src/routes/Commit.js
+++ src/routes/Commit.js
@@ -1,28 +1,74 @@
 import React from "react";
-import Button from "../components/button";
 import { Container, Row, Carousel, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "../components/Header.js";
 import Footer from "../components/Footer.js";
 import Card from "../components/card";
-import tmp from "../scripts/tmp.json";
+import useJsonFile from "../hooks/useJsonFile";
+import { eel } from "../eel.js";
+import { useEffect, useState } from "react";
+// import tmp from "../scripts/tmp.json";
 
 const Commit = () => {
+  const [jsonData, setJsonData] = useJsonFile("./tmp.json");
+  const [message, setMessage] = useState("");
+  const [info, setInfo] = useState([]);
+  const dirValue = localStorage.getItem("dirValue");
+
+  useEffect(() => {
+    setInfo(localStorage.getItem("repoInfo").split(","));
+  }, []);
+
+  const handleSubmit = (file, message, event) => {
+    event.preventDefault();
+    console.log(info, jsonData, message);
+    if (event.target.name === "discard") {
+      setMessage("-r");
+    }
+    const [url, branch] = info;
+    eel.commitAndUpdate(
+      dirValue,
+      file,
+      message,
+      url,
+      branch
+    )((ret) => {
+      console.log(ret)
+      ret ? console.log("Pushed!") : console.log("Reverted");
+    });
+  };
+
+  const handleChange = (event) => {
+    setMessage(event.target.value);
+  };
   return (
     <>
       <Header />
       <Container>
         <Row>
           <Col lg={"12"}>
-            <Carousel indicators={false}>
-              {tmp.map((file,idx) => {
-                return (
-                  <Carousel.Item key={idx}>
-                    <Card key={idx} heading={file.path.split('\\').pop()} content={file.changes} />
-                  </Carousel.Item>
-                );
-              })}
-            </Carousel>
+            {jsonData.length ? (
+              <Carousel indicators={false}>
+                {jsonData.map((file, idx) => {
+                  return (
+                    <Carousel.Item key={idx}>
+                      <Card
+                        key={idx}
+                        heading={file.path.split("\\").pop()}
+                        content={file.changes}
+                        onChange={handleChange}
+                        onSubmit={(e) => {
+                          handleSubmit(file.path, message, e);
+                        }}
+                        value={message}
+                      />
+                    </Carousel.Item>
+                  );
+                })}
+              </Carousel>
+            ) : (
+              <h1 style={{ opacity: "0.5" }}>All set! No changes to push</h1>
+            )}
           </Col>
         </Row>
       </Container>
