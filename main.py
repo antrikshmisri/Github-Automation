8d6b2801d71260a99e6e9b9c41c79f8efc1c2f4c
antrikshmisri <antrikshmisri@gmail.com>
2021-03-15 12:28:47 +0530

    feat: run functions on seperate threads

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import eel
 import sys
 from src.scripts import repoInfo, utils
-
+import os
+import threading
 
 @eel.expose
 def getpara(arg):
@@ -23,18 +24,20 @@ def checkPath(path):
 @eel.expose
 def init(url, branch, path):
     from src.scripts import main
-    main.init(url, branch, path)
+    t1 = threading.Thread(target=main.init , args=(url, branch, path)) 
+    t1.start()
 
 @eel.expose
-def commitAndUpdate(path , file , url , branch):
+def commitAndUpdate(path , file , msg , url , branch):
     from src.scripts.utils import commitAndUpdate
-    commitAndUpdate(path , file , url , branch)
+    t2 = threading.Thread(target=commitAndUpdate , args=(path , file , msg , url , branch))
+    t2.start()
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == '--develop':
-            eel.init('client')
+            eel.init('src')
             eel.start({
                 'port': 3000,
             }, options={
