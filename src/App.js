16f45fded8e9afd7646ef3d53356a47dcf15bf61
antrikshmisri <antrikshmisri@gmail.com>
2021-03-22 17:25:45 +0530

    fix: removed unused imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { eel } from "./eel.js";
 import Splash from "./routes/Splash";
