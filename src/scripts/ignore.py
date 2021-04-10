ab10f56f420faea595aa9e4986db377258a4771e
antrikshmisri <antrikshmisri@gmail.com>
2021-04-10 14:52:14 +0530

    feat: make .gitignore if not found in path

--- src/scripts/ignore.py
+++ src/scripts/ignore.py
@@ -1,15 +1,23 @@
-import os
+from os.path import join, isfile, isdir
 
+def makeIgnore(path):
+    f = open(join(path , '.gitignore') , "x")
+    f.write('.git\n.idea\nnode_modules\n__pycache__\n')
+    f.close()
 
 def getIgnoreFiles(path):
-    ignorepath = os.path.join(path, '.gitignore')
+
+    if not(isfile(join(path, '.gitignore'))):
+        makeIgnore(path)
+
+    ignorepath = join(path, '.gitignore')
     ignorefiles = []
     with open(ignorepath) as ignore:
         files = ignore.readlines()
         for file in files:
             file = ''.join(file.splitlines())
             if(file != ''):
-                filepath = os.path.join(path , file)
-                if(os.path.isfile(filepath) or os.path.isdir(filepath)):
+                filepath = join(path , file)
+                if(isfile(filepath) or isdir(filepath)):
                     ignorefiles.append(file)
     return ignorefiles
