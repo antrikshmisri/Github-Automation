b0118ddcef60329e8439aba6ca24b42f9c90dd28
antrikshmisri <antrikshmisri@gmail.com>
2021-04-10 14:49:41 +0530

    fix: switched to hash router for file based reqs

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { eel } from "./eel.js";
 import Splash from "./routes/Splash";
 import Home from "./routes/Home";
