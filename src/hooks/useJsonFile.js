1e17230bcdfed6181d8f4358c72f99c10fb821c7
antrikshmisri <antrikshmisri@gmail.com>
2021-04-21 20:03:14 +0530

    feat: get json file from process.env

--- src/hooks/useJsonFile.js
+++ src/hooks/useJsonFile.js
@@ -3,12 +3,13 @@ import header from "../constants/headers"
 const useJsonFile = (fileName) => {
   const [jsonData, setJsonData] = useState([]);
   useEffect(() => {
-    fetch(fileName , header)
+    fetch(`${process.env.PUBLIC_URL}/${fileName}` , header)
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setJsonData(data);
+        console.log(jsonData)
       });
   }, []);
 
