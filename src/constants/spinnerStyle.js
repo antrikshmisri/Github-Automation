3376bbac89c6d2c224e7ad23d58890bb802421a0
antrikshmisri <antrikshmisri@gmail.com>
2021-04-24 18:08:13 +0530

    feat: styling for spinner

---
+++ src/constants/spinnerStyle.js
@@ -0,0 +1,10 @@
+import { css } from "@emotion/core";
+
+const spinnetStyle = css`
+  z-index: 10;
+  position: absolute;
+  top: calc(50% - 7.5px);
+  left: calc(50% - 2.5px)
+`
+
+export default spinnetStyle
