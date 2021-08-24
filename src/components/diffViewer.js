6c8747b6a5839db909888e8b7d69b30a082e7b1f
antrikshmisri <antrikshmisri@gmail.com>
2021-08-24 14:59:01 +0530

    feat: added diffViewer component

---
+++ src/components/diffViewer.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+
+const DiffViewer = ({ diff, setShowDiffContainer }) => {
+  const diffLines = diff.length?diff.split("\n"):[];
+
+  return (
+    <div className={"diffContainer code-block"}>
+      <button
+        className={"close-btn"}
+        onClick={() => setShowDiffContainer(false)}
+      >
+        <img src="https://image.flaticon.com/icons/png/512/1828/1828778.png" className={'btn-icon'}/>
+      </button>
+      {diffLines.map((line, idx) => {
+        let lineColor = "diff-line";
+
+        if (line.startsWith("+")) {
+          lineColor = "diff-line-added";
+        } else if (line.startsWith("-")) {
+          lineColor = "diff-line-remove";
+        }
+
+        return (
+          <code key={idx} id={lineColor}>
+            {line}
+          </code>
+        );
+      })}
+    </div>
+  );
+};
+
+export default DiffViewer;
